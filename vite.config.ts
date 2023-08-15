c7bcd7ca9853d50361e411207a88f51f11f0d250
Yakov Wildfluss <yakov@wildfluss.com>
2023-08-15 16:55:15 +0300

    add vite-plugin-singlefile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteSingleFile({
+      removeViteModuleLoader: true,
+      useRecommendedBuildConfig: true,
+    }),
+  ],
+  build: {
+    outDir: 'build',
+    target: 'es2019',
+  },
 })
